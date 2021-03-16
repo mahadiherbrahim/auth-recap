93a2d19f7a03986ee0f8f98d2b1b3b74bd69bcd1
Mahadi H Ebrahim <mahadihebrahim@hotmail.com>
2021-03-16 11:56:13 +0600

    Auth Recap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import firebaseConfig from "./firebase.config";
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 function App() {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    photo: ''
+  });
+
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+
+
+  const handleGoogleSignIn = () => {
+    firebase.auth().signInWithPopup(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user)
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage);
+      });
+
+  }
+  const handleFacebookSignIn = () => {
+    firebase
+      .auth().signInWithPopup(facebookProvider)
+      .then((result) => {
+        const user = result.user;  
+        setUser(user)
+        console.log(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        console.log(errorMessage, errorMessage, email);
+      });
+  }
+
+  const handleGithubSignIn = () => {
+    firebase
+      .auth().signInWithPopup(githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user)
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Login Logout System</h2>
+      <button onClick={handleGoogleSignIn}> Sign With Google </button>
+      <br></br>
+      <button onClick={handleFacebookSignIn}> Sign In With Facebook </button>
+      <br></br>
+      <button onClick={handleGithubSignIn}>Sign In With GitHub</button>
+      <div>
+          <h3>Name:{user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" style={{ width: '200px' }} />
+        </div>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA985I_6pswCPeKKM1vD5Kd0alKuBCco5A",
+    authDomain: "auth-recap-929b5.firebaseapp.com",
+    projectId: "auth-recap-929b5",
+    storageBucket: "auth-recap-929b5.appspot.com",
+    messagingSenderId: "316306936188",
+    appId: "1:316306936188:web:0b9235059ca82f5ca554f9"
+  };
+
+export default firebaseConfig;
